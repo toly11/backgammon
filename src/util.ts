16728ad7a53f3e1447e493f5cba0963fbd364461
user <user@domain.com>
2022-02-20 02:11:33 +0200

    temp fix for util UntilRange

--- src/util.ts
+++ src/util.ts
@@ -1,13 +1,16 @@
+export type UntilRange<Max extends number> = number | Max
+
+// todo fix this type generator (not working)
 // usage example: UntilRange<25>
 // result: 0 | 1 | 2 ... | 24
-export type UntilRange<Max extends number> = number extends Max
-  ? number
-  : _Range<Max, []>;
+// export type UntilRange<Max extends number> = number extends Max
+//   ? number
+//   : _Range<Max, []>;
 
-type _Range<Max extends number, Arr extends number[]>
-  = Arr['length'] extends Max
-  ? Arr[number]
-  : _Range<Max, [...Arr, Arr['length']]>;
+// type _Range<Max extends number, Arr extends number[]>
+//   = Arr['length'] extends Max
+//   ? Arr[number]
+//   : _Range<Max, [...Arr, Arr['length']]>;
 
 
 // todo write tests
